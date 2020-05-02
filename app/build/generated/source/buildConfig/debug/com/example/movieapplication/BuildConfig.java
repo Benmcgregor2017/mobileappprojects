7fa6be898170610f611bd0cccc3855b4cf707b87
Ryan-McGregor <benmcgregor1999@gmail.com>
2020-05-02 16:30:54 -0400

    App as of 5/2/2020 4:30 PM
    Properly set up Github (Yay). Activities all laid out. Working on realtime database now.

---
+++ app/build/generated/source/buildConfig/debug/com/example/movieapplication/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.movieapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.movieapplication";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
